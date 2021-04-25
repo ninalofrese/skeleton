af80856406d17d805a7057019754376a0680fb03
Nina Lofrese <nina.lofrese@gmail.com>
2021-04-25 12:53:54 -0300

    Revert "Template cleanup"
    This reverts commit ed30f41da15d23dee44b3a66ea5f7ace7e863e7c.

--- buildSrc/src/main/java/Coordinates.kt
+++ buildSrc/src/main/java/Coordinates.kt
@@ -1,7 +1,7 @@
-const val PUBLISHING_GROUP = "com.github.ninalofrese.skeleton"
+const val PUBLISHING_GROUP = "br.dev.nina.skeleton"
 
 object AppCoordinates {
-    const val APP_ID = "com.github.ninalofrese.skeleton.app"
+    const val APP_ID = "br.dev.nina.skeleton.app"
 
     const val APP_VERSION_NAME = "1.0.0"
     const val APP_VERSION_CODE = 1
